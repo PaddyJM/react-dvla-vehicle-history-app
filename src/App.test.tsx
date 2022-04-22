e9bc465627605cc7811765a7cc0c3ab43705d78c
Patrick Morton <patrickjamesmorton@gmail.com>
2022-04-22 11:10:54 +0100

    updated tests to use proper sync and async functions

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,40 +7,39 @@ import App from "./App";
 it("renders the MOT history when clicked on", async () => {
   render(<App />);
 
-  const button = await screen.findByText("Check MOT history.");
-  const input = await screen.findByRole("textbox");
+  const button = screen.getByText("Check MOT history.");
+  const input = screen.getByRole("textbox");
 
   fireEvent.input(input, { target: { value: "OY02UHE" } });
   fireEvent.click(button);
 
-  expect(screen.findByText("Registration: OY02UHE")).toBeTruthy();
+  expect(await screen.findByText("Registration: OY02UHE")).toBeTruthy();
 });
 
 it("removes all whitespace from registration and sets it to uppercase", async () => {
   const funkyRegistration = " oy02 uhe ";
 
   render(<App />);
-  const input = await screen.findByRole("textbox");
-  const button = await screen.findByText("Check MOT history.");
+  const input = screen.getByRole("textbox");
+  const button = screen.getByText("Check MOT history.");
 
   fireEvent.input(input, { target: { value: funkyRegistration } });
   fireEvent.click(button);
 
-  expect(screen.findByText("Registration: OY02UHE")).toBeTruthy();
+  expect(await screen.findByText("Registration: OY02UHE")).toBeTruthy();
 });
 
 it("resets mot history on second submit", async () => {
   const firstRegistration = "OY02UHE";
-  const secondRegistration = "MA10LAO";
 
   render(<App />);
-  const input = await screen.findByRole("textbox");
-  const button = await screen.findByText("Check MOT history.");
+  const input = screen.getByRole("textbox");
+  const button = screen.getByText("Check MOT history.");
 
   fireEvent.input(input, { target: { value: firstRegistration } });
   fireEvent.click(button);
 
-  expect(screen.findByText("Registration: OY02UHE")).toBeTruthy();
+  expect(await screen.findByText("Registration: OY02UHE")).toBeTruthy();
 
   fireEvent.input(input, { target: { value: "foo" } });
   fireEvent.click(button);
